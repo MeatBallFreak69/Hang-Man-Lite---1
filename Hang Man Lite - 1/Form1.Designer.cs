202fa5c45597351a8e1724be322045eb8c4c5a9b
Clark Miller <glitchclick@gmail.com>
2021-03-20 14:26:17 -0400

    Add project files.

---
+++ Hang Man Lite - 1/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace Hang_Man_Lite___1
+{
+    partial class frmHangman
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGreeting = new System.Windows.Forms.Label();
+            this.lblLetters = new System.Windows.Forms.Label();
+            this.lstGuessedWords = new System.Windows.Forms.ListBox();
+            this.lblUsedLetters = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.imgHang = new System.Windows.Forms.PictureBox();
+            this.lblWord = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgHang)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblGreeting
+            // 
+            this.lblGreeting.AutoSize = true;
+            this.lblGreeting.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGreeting.Location = new System.Drawing.Point(13, 13);
+            this.lblGreeting.Name = "lblGreeting";
+            this.lblGreeting.Size = new System.Drawing.Size(327, 29);
+            this.lblGreeting.TabIndex = 0;
+            this.lblGreeting.Text = "Welcome to Hang-Man Lite";
+            // 
+            // lblLetters
+            // 
+            this.lblLetters.AutoSize = true;
+            this.lblLetters.Location = new System.Drawing.Point(14, 43);
+            this.lblLetters.Name = "lblLetters";
+            this.lblLetters.Size = new System.Drawing.Size(315, 13);
+            this.lblLetters.TabIndex = 1;
+            this.lblLetters.Text = "Enter a letter to reveal the hidden word. 3 strikes and you are out!";
+            // 
+            // lstGuessedWords
+            // 
+            this.lstGuessedWords.FormattingEnabled = true;
+            this.lstGuessedWords.Location = new System.Drawing.Point(278, 98);
+            this.lstGuessedWords.Name = "lstGuessedWords";
+            this.lstGuessedWords.Size = new System.Drawing.Size(120, 173);
+            this.lstGuessedWords.TabIndex = 3;
+            // 
+            // lblUsedLetters
+            // 
+            this.lblUsedLetters.AutoSize = true;
+            this.lblUsedLetters.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsedLetters.Location = new System.Drawing.Point(278, 76);
+            this.lblUsedLetters.Name = "lblUsedLetters";
+            this.lblUsedLetters.Size = new System.Drawing.Size(96, 16);
+            this.lblUsedLetters.TabIndex = 4;
+            this.lblUsedLetters.Text = "Used Letters";
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(278, 279);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(36, 20);
+            this.txtGuess.TabIndex = 5;
+            this.txtGuess.TextChanged += new System.EventHandler(this.txtGuess_TextChanged);
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.Location = new System.Drawing.Point(320, 277);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(75, 23);
+            this.btnGuess.TabIndex = 6;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // imgHang
+            // 
+            this.imgHang.Image = global::Hang_Man_Lite___1.Properties.Resources.hangman_empty;
+            this.imgHang.Location = new System.Drawing.Point(12, 76);
+            this.imgHang.Name = "imgHang";
+            this.imgHang.Size = new System.Drawing.Size(260, 223);
+            this.imgHang.TabIndex = 2;
+            this.imgHang.TabStop = false;
+            // 
+            // lblWord
+            // 
+            this.lblWord.AutoSize = true;
+            this.lblWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWord.Location = new System.Drawing.Point(8, 303);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(338, 55);
+            this.lblWord.TabIndex = 7;
+            this.lblWord.Text = "_ _ _ _ _ _ _ _";
+            // 
+            // frmHangman
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 371);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.lblUsedLetters);
+            this.Controls.Add(this.lstGuessedWords);
+            this.Controls.Add(this.imgHang);
+            this.Controls.Add(this.lblLetters);
+            this.Controls.Add(this.lblGreeting);
+            this.Name = "frmHangman";
+            this.Text = "HangMan";
+            this.Load += new System.EventHandler(this.frmHangman_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgHang)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGreeting;
+        private System.Windows.Forms.Label lblLetters;
+        private System.Windows.Forms.PictureBox imgHang;
+        private System.Windows.Forms.ListBox lstGuessedWords;
+        private System.Windows.Forms.Label lblUsedLetters;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Label lblWord;
+    }
+}
+
---
+++ Hang Man Lite - 1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hang_Man_Lite___1
+{
+    public partial class frmHangman : Form
+    {
+        string word;
+        string displayWord;
+        int guessCounter = 0;
+        List<string> letters = new List<string>();
+
+        public frmHangman()
+        {
+            InitializeComponent();
+        }
+
+        private void frmHangman_Load(object sender, EventArgs e)
+        {
+            word = "COMPUTER";
+            displayWord = "_ _ _ _ _ _ _ _";
+            lblWord.Text = displayWord;
+            lstGuessedWords.DataSource = letters;
+        }
+
+        private void txtGuess_TextChanged(object sender, EventArgs e)
+        {
+            txtGuess.Text = txtGuess.Text.ToUpper();
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            string toFind = txtGuess.Text;
+            int index = word.IndexOf(txtGuess.Text);
+            if (index == -1)
+            {
+                letters.Add(word);
+                lstGuessedWords.DataSource = null;
+                lstGuessedWords.DataSource = letters;
+            }
+            lblLetters.Text = $"Found '{toFind}' in '{word}' at position {index}";
+        }
+    }
+}
